fa3325d6cda0fcfed90b8d3daf4c4c6247aafbb0
EC2 Default User <ec2-user@ip-172-31-85-248.ec2.internal>
2025-11-25 22:58:51 +0000

    new

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "clc14-marcio-terraform"
+    bucket = "clc14-marcio-terraform-1"
     key    = "network/terraform.tfstate"
     region = "us-east-1"
     use_lockfile = true
